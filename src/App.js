fc88db026c09210b6200da49744c8bf631c08b65
Bagus <gusdik@geekseat.com.au>
2019-08-02 14:51:24 +0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
+
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HTAS PAGE</h1>
+  </div> 
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div> 
+);
+
 function App() {
   return (
   <div>
-    <HomePage />
+    {/* <HomePage /> */}
+    <div>
+      <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+      <Route path='/topics/:topicID' component={TopicDetail} />
+      </Switch>
+    </div>
   </div>
   );
 }
